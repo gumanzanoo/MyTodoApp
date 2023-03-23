7e07e9fb06ebc08b789533d0dc0f49b068a69954
Gustavo Manzano <74085699+gumanzanoo@users.noreply.github.com>
2023-03-23 16:13:28 -0300

    CodeGeneration todo scaffold

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyTodoApp.Models;
 
 namespace MyTodoApp.Data
 {
@@ -12,5 +13,7 @@ namespace MyTodoApp.Data
             : base(options)
         {
         }
+
+        public DbSet<Todo> Todos { get; set; }
     }
 }
